471bf742efa969a16362539159d0e04a548ce041
aambadkar <khodaskar.ankita@gmail.com>
2023-10-02 11:57:27 +0300

    expense script

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -15,3 +15,39 @@ resource "aws_route53_record" "frontend" {
   ttl     = 30
   records = [aws_instance.frontend.private_ip]
 }
+
+resource "aws_instance" "backend" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+  vpc_security_group_ids =["sg-0d40656ca88a46995"]
+
+  tags = {
+    Name = "backend"
+  }
+}
+
+resource "aws_route53_record" "backend" {
+  zone_id = "Z05576924GZ1MK3S5OBT"
+  name    = "backend.devopsa17.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.backend.private_ip]
+}
+
+resource "aws_instance" "mysql" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+  vpc_security_group_ids =["sg-0d40656ca88a46995"]
+
+  tags = {
+    Name = "mysql"
+  }
+}
+
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z05576924GZ1MK3S5OBT"
+  name    = "mysql.devopsa17.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
+}

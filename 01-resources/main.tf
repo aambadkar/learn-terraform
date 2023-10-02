16b60949e4f23fcefc958ab4d10dae3f71078437
aambadkar <khodaskar.ankita@gmail.com>
2023-10-02 11:00:50 +0300

    expense script

--- 01-resources/main.tf
+++ 01-resources/main.tf
@@ -6,3 +6,11 @@ resource "aws_instance" "web" {
     Name = "HelloWorld"
   }
 }
+
+resource "aws_route53_record" "www" {
+  zone_id = "Z05576924GZ1MK3S5OBT"
+  name    = "test.devopsa17.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.web.private_ip]
+}
